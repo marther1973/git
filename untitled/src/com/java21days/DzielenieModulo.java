4a8300b54008c5e193d13717c9392d6135b920b3
marther1973 <marther@o2.pl>
2018-12-07 01:07:22 +0100

    dzień 1 i 2

---
+++ untitled/src/com/java21days/DzielenieModulo.java
@@ -0,0 +1,13 @@
+package com.java21days;
+
+    public class DzielenieModulo{
+        public static void main(String[] args) {
+            int a = 10;
+            int b = 3;
+
+            int dzielenie = a/b;
+            int modulo = a%b;
+
+            System.out.println( dzielenie + "\t" + modulo );
+        }
+    }
